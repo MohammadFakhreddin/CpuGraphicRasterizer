895a1accdda88a1bdb513f943f27fac345e78d8e
Mohammad F <mohammad.fakhreddin@gmail.com>
2020-04-07 18:24:24 +0430

    Unit test support is complete

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Projects/3d-cube
+# Build directory: C:/Projects/3d-cube
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit-test "unit-test")
+set_tests_properties(unit-test PROPERTIES  _BACKTRACE_TRIPLES "C:/Projects/3d-cube/CMakeLists.txt;168;add_test;C:/Projects/3d-cube/CMakeLists.txt;0;")
